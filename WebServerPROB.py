755596feb756bd22229ff7d4b655caac37fccfdc
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-10-03 13:41:00 -0400

    Updated to working for binary

--- WebServerPROB.py
+++ WebServerPROB.py
@@ -1,9 +1,9 @@
 # Import socket module
-from socket import *    
+from socket import *
 
 # Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
+# (AF_INET is used for IPv4 protocols)
+# (SOCK_STREAM is used for TCP)
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
@@ -13,45 +13,47 @@ HOST = '127.0.0.1'
 PORT = 32008
 serverSocket.bind((HOST, PORT))
 serverSocket.listen()
-# Fill in end 
+# Fill in end
 
 # Server should be up and running and listening to the incoming connections
 while True:
     print('Ready to serve...')
-    
+
     # Set up a new connection from the client
-    connectionSocket, addr = serverSocket.accept() #Fill in start             #Fill in end
-    
+    connectionSocket, addr = serverSocket.accept()  # Fill in start             #Fill in end
+
     # If an exception occurs during the execution of try clause
     # the rest of the clause is skipped
     # If the exception type matches the word after except
     # the except clause is executed
     try:
         # Receives the request message from the client
-        message =  connectionSocket.recv(1024) #Fill in start           #Fill in end
+        message = connectionSocket.recv(1024)  # Fill in start           #Fill in end
 
         # Extract the path of the requested object from the message
         # The path is the second part of HTTP header, identified by [1]
         filename = message.split()[1]
 
-        # Because the extracted path of the HTTP request includes 
-        # a character '\', we read the path from the second character 
-        f = open(filename[1:])
+        # Because the extracted path of the HTTP request includes
+        # a character '\', we read the path from the second character
+        f = open(filename[1:], "rb")
 
-        # Store the entire contenet of the requested file in a temporary buffer
-        outputdata = f.read() #Fill in start         #Fill in end
+        # Store the entire content of the requested file in a temporary buffer
+        outputdata = f.read()
+
+        # close the file
+        f.close()
 
         # Send the HTTP response header line to the connection socket
-        # Fill in start
         # Connection is successful if it gets here
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-            # Fill in end
- 
+
         # Send the content of the requested file to the connection socket
-        for i in range(0, len(outputdata)):  
-            connectionSocket.send(outputdata[i].encode())
+        connectionSocket.sendall(outputdata)
+        # for i in range(0, len(outputdata)):
+        #    connectionSocket.send(outputdata[i])
         connectionSocket.send("\r\n".encode())
-        
+
         # Close the client connection socket
         connectionSocket.close()
 
@@ -59,12 +61,11 @@ while True:
         # Send HTTP response message for file not found
         # Fill in start
         connectionSocket.send("HTTP/1.1 404 Not found\r\n\r\n".encode())
-            # Fill in end
-        
+        # Fill in end
+
         # Close the client connection socket
         # Fill in start
         connectionSocket.close()
-            # Fill in end
+    # Fill in end
 
-serverSocket.close()  
-
+serverSocket.close()
